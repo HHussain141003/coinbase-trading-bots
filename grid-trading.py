be858d120d90aff7c16ea33e7e4f7190acf6f76d
Hanan Hussain <hhussain141003@gmail.com>
2024-11-02 21:48:55 +0000

    Added function get_current_price()

--- grid-trading.py
+++ grid-trading.py
@@ -1,4 +1,5 @@
 import os
+import requests
 import logging
 from dotenv import load_dotenv
 from coinbase_advanced_trader.enhanced_rest_client import EnhancedRESTClient
@@ -12,3 +13,14 @@ api_key = os.getenv('name')
 api_secret = os.getenv('privateKey')
 
 client = EnhancedRESTClient(api_key, api_secret)
+
+def get_current_price():
+    try:
+        response = requests.get('https://api.coinbase.com/v2/prices/spot?currency=BTC')
+        response.raise_for_status()
+        data = response.json()
+        print(data)
+    except Exception as e:
+        logger.info("Failed to retrieve current price")
+
+get_current_price()
