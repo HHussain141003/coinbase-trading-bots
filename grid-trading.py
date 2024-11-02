d7a0dc118fcb1a5e925b396a98cd84fc9f193099
Hanan Hussain <hhussain141003@gmail.com>
2024-11-02 22:14:15 +0000

    Changed requests for get_current_price()

--- grid-trading.py
+++ grid-trading.py
@@ -16,7 +16,7 @@ client = EnhancedRESTClient(api_key, api_secret)
 
 # Bot Settings:
 ORDER_AMOUNT = 20
-COIN = "BTC"
+COIN = "ETH"
 TRADING_PAIR = f"{COIN}-USDC"
 UPPER_LIMIT = 0
 LOWER_LIMIT = 0
@@ -24,11 +24,13 @@ TIMER = 300
 
 def get_current_price():
     try:
-        response = requests.get(f'https://api.coinbase.com/v2/prices/spot?currency={COIN}')
+        response = requests.get(f"https://api.exchange.coinbase.com/currencies/{COIN}")
         response.raise_for_status()
         data = response.json()
         print(data)
+        # return float(data['data']['amount'])
     except Exception as e:
-        logger.info("Failed to retrieve current price")
+        logger.warning(f"Failed to retrieve current price for {COIN}")
 
-get_current_price()
+if __name__ == "__main__":
+    get_current_price()
