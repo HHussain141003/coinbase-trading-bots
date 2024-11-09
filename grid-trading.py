56a2a009c582b83cdd38f500b02541aa448f7a18
Hanan Hussain <hhussain141003@gmail.com>
2024-11-09 21:17:29 +0000

    Added place_buy_order() function

--- grid-trading.py
+++ grid-trading.py
@@ -14,14 +14,15 @@ api_secret = os.getenv('privateKey')
 client = RESTClient(api_key, api_secret)
 
 # Bot Settings:
-COIN = "BTC"
-TRADING_PAIR = f"{COIN}-USD"
-UPPER_BOUND = 0
-LOWER_BOUND = 0
-GRID_SIZE = 0            
+COIN = "XRP"
+TRADING_PAIR = f"{COIN}-USDC"
+GRID_UPPER_BOUND = 0.5557
+GRID_LOWER_BOUND = 0.5458
+GRID_SIZE = 20            
 ORDER_SIZE_USD = 0    
 TIMER = 0 
 
+price_interval = (GRID_UPPER_BOUND - GRID_LOWER_BOUND) / GRID_SIZE
 
 def get_current_price():
     try:
@@ -31,6 +32,9 @@ def get_current_price():
     except Exception as e:
         logger.warning(f"Failed to retrieve current price for {TRADING_PAIR}")
         return None
+
+def place_buy_order():
+    order = client.market_order_buy(product_id=TRADING_PAIR, quote_size=ORDER_SIZE_USD)
 
 def grid_bot():
     while True:
